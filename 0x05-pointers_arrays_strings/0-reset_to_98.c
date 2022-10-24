d6f13da03312eaa9cb54746916271b09cb22bd63
Maureensunny <nwankpamaureen2@gmail.com>
2022-10-24 03:39:41 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+	void reset_to_98(int *n);
+{
+	int n;
+
+	n = 450
+		printf("n = %d\n", n);
+		reset_to_98($n);
+		printf("n = %d\n", n);
+		Return(0);
+}
